575fbefdb523698834a702769863a4a704d94d85
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 13:29:44 +0500

    Create Closer.java

---
+++ allinterface/src/main/java/org/example/test24/allinterface/Closer.java
@@ -0,0 +1,33 @@
+package org.example.test24.allinterface;
+
+import org.example.test24.allinterface.commPort.CommPort_Interface;
+import org.example.test24.allinterface.runner.Runner_Interface;
+import org.example.test24.allinterface.screen.ScreenClass_interface;
+
+public class Closer {
+    private static Closer closer = new Closer();
+
+    private CommPort_Interface      commPort    = null;
+    private Runner_Interface        runner      = null;
+    private ScreenClass_interface   mainFx      = null;
+
+    public static Closer getCloser() {
+        return closer;
+    }
+
+    public void init(
+        CommPort_Interface commPort,
+        Runner_Interface runner,
+        ScreenClass_interface mainFx
+    ) {
+        this.commPort = commPort;
+        this.runner = runner;
+        this.mainFx = mainFx;
+    }
+
+    public void closeAll() {
+        commPort.Close();
+        runner.Close();
+        mainFx.exitApp();
+    }
+}
