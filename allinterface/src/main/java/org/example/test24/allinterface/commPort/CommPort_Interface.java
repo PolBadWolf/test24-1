c889ed4fe17963f6190a131400ab0b036b73bad7
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 09:25:35 +0500

    add all interface comm port

---
+++ allinterface/src/main/java/org/example/test24/allinterface/commPort/CommPort_Interface.java
@@ -0,0 +1,12 @@
+package org.example.test24.allinterface.commPort;
+
+public interface CommPort_Interface {
+    String[] getListPortsName();
+    int Open(Runner_Interface runner, String portName, BAUD baud);
+    final int INITCODE_OK           = 0;
+    final int INITCODE_NOTEXIST     = 1;
+    final int INITCODE_ERROROPEN    = 2;
+    void Close();
+    boolean ReciveStart();
+    void ReciveStop();
+}
