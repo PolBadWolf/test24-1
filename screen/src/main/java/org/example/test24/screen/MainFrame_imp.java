e972d06303e74872981a8a3b8538398b0e4a16c6
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 13:02:51 +0500

    create screen impl

---
+++ screen/src/main/java/org/example/test24/screen/MainFrame_imp.java
@@ -0,0 +1,6 @@
+package org.example.test24.screen;
+
+import org.example.test24.allinterface.screen.MainFrame_interface;
+
+public interface MainFrame_imp extends MainFrame_interface {
+}
---
+++ screen/src/main/java/org/example/test24/screen/ScreenClass_impl.java
@@ -0,0 +1,6 @@
+package org.example.test24.screen;
+
+import org.example.test24.allinterface.screen.ScreenClass_interface;
+
+public interface ScreenClass_impl extends ScreenClass_interface {
+}
