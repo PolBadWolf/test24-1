4ec544cc297824575148d5db4a984a6cda91807e
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 10:10:44 +0500

    commport getListPortsName

--- RS232/src/main/java/org/example/test24/RS232/CommPort.java
+++ RS232/src/main/java/org/example/test24/RS232/CommPort.java
@@ -1,4 +1,37 @@
 package org.example.test24.RS232;
 
+import com.fazecast.jSerialComm.SerialPort;
+import org.example.test24.allinterface.commPort.BAUD;
+import org.example.test24.allinterface.runner.Runner_Interface;
+
 public class CommPort implements CommPort_Impl {
+    @Override
+    public String[] getListPortsName() {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        String[] namePorts = new String[ports.length];
+        for (int i = 0; i < ports.length; i++) {
+            namePorts[i] = ports[i].getSystemPortName().toUpperCase();
+        }
+        return namePorts;
+    }
+
+    @Override
+    public int Open(Runner_Interface runner, String portName, BAUD baud) {
+        return 0;
+    }
+
+    @Override
+    public void Close() {
+
+    }
+
+    @Override
+    public boolean ReciveStart() {
+        return false;
+    }
+
+    @Override
+    public void ReciveStop() {
+
+    }
 }
--- RS232/src/main/java/org/example/test24/RS232/CommPort_Impl.java
+++ RS232/src/main/java/org/example/test24/RS232/CommPort_Impl.java
@@ -1,4 +1,6 @@
 package org.example.test24.RS232;
 
-public interface CommPort_Impl {
+import org.example.test24.allinterface.commPort.CommPort_Interface;
+
+public interface CommPort_Impl extends CommPort_Interface {
 }
