23df9c4b5c97b391e7e4714b849b0e301650fa75
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 12:58:40 +0500

    create screen & frame interface in allinterface

---
+++ allinterface/src/main/java/org/example/test24/allinterface/screen/MainFrame_interface.java
@@ -0,0 +1,12 @@
+package org.example.test24.allinterface.screen;
+
+import javafx.scene.canvas.Canvas;
+
+public interface MainFrame_interface {
+    void canvas_Clear();
+    void canvas_Line();
+    void label1_txt(String text);
+    int getHeighCanvas();
+    Canvas getCanvas1();
+    void label2_txt(String text);
+}
---
+++ allinterface/src/main/java/org/example/test24/allinterface/screen/ScreenClass_interface.java
@@ -0,0 +1,5 @@
+package org.example.test24.allinterface.screen;
+
+public interface ScreenClass_interface {
+    void exitApp();
+}
