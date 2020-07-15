24b8ee1f4710d016edad07ee8e2ee9f3e2dfd95d
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 12:59:22 +0500

    Update Runner_Interface.java

--- allinterface/src/main/java/org/example/test24/allinterface/runner/Runner_Interface.java
+++ allinterface/src/main/java/org/example/test24/allinterface/runner/Runner_Interface.java
@@ -1,6 +1,7 @@
 package org.example.test24.allinterface.runner;
 
 import org.example.test24.allinterface.commPort.CommPort_Interface;
+import org.example.test24.allinterface.screen.MainFrame_interface;
 
 public interface Runner_Interface {
     void init(CommPort_Interface commPort, MainFrame_interface mainFrame);
