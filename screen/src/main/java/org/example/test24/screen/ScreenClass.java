ceb621de0b9bd2be744f4b919a69fbf9a1c807db
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 13:19:59 +0500

    Create ScreenClass.java

---
+++ screen/src/main/java/org/example/test24/screen/ScreenClass.java
@@ -0,0 +1,49 @@
+package org.example.test24.screen;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ScreenClass extends Application implements ScreenClass_impl, Runnable {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = null;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainFrame.fxml"));
+
+        try {
+            root = (Parent) loader.load();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("title fx window");
+        primaryStage.show();
+    }
+
+    @Override
+    public void exitApp() {
+        Platform.exit();
+    }
+
+    @Override
+    public void run() {
+        ScreenClass.main(null);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+}
