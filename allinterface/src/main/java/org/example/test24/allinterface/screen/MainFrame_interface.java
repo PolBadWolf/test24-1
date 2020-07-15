411328c89c1acba7f33a41f941c54446c1be2b24
PolBadWolf <gm.gameoff@gmail.com>
2020-07-15 14:01:36 +0500

    update MainFrame

--- allinterface/src/main/java/org/example/test24/allinterface/screen/MainFrame_interface.java
+++ allinterface/src/main/java/org/example/test24/allinterface/screen/MainFrame_interface.java
@@ -1,12 +1,13 @@
 package org.example.test24.allinterface.screen;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 
 public interface MainFrame_interface {
-    void canvas_Clear();
-    void canvas_Line();
+    void canvas_Clear(Color backColor);
+    void canvas_Line(Color lineColor, double lineWidth, double x0, double y0, double x, double y);
     void label1_txt(String text);
-    int getHeighCanvas();
+    double getHeighCanvas();
     Canvas getCanvas1();
     void label2_txt(String text);
 }
--- screen/src/main/java/org/example/test24/screen/MainFrame.java
+++ screen/src/main/java/org/example/test24/screen/MainFrame.java
@@ -1,4 +1,69 @@
 package org.example.test24.screen;
 
-public class MainFrame {
+import javafx.application.Platform;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFrame implements Initializable, MainFrame_imp {
+    public static MainFrame mainFrame = null;
+
+    private GraphicsContext gc = null;
+    public Canvas canvas;
+
+    public Label label1;
+    public Label label2;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mainFrame = this;
+        gc = canvas.getGraphicsContext2D();
+    }
+
+    @Override
+    public void canvas_Clear(Color backColor) {
+        Platform.runLater( ()->{
+            gc.setFill(backColor);
+            gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            gc.stroke();
+        });
+    }
+
+    @Override
+    public void canvas_Line(Color lineColor, double lineWidth, double x0, double y0, double x, double y) {
+        Platform.runLater( ()->{
+            gc.beginPath();
+            gc.setStroke(lineColor);
+            gc.setLineWidth(lineWidth);
+            gc.moveTo(x0, y0);
+            gc.lineTo(x, y);
+            gc.stroke();
+            gc.closePath();
+        });
+    }
+
+    @Override
+    public void label1_txt(String text) {
+        Platform.runLater( ()->label1.setText(text) );
+    }
+
+    @Override
+    public double getHeighCanvas() {
+        return canvas.getHeight();
+    }
+
+    @Override
+    public Canvas getCanvas1() {
+        return canvas;
+    }
+
+    @Override
+    public void label2_txt(String text) {
+        Platform.runLater( ()->label2.setText(text) );
+    }
 }
