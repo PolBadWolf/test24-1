89627d8a977761d8eaa226af3514509106126015
PolBadWolf <gm.gameoff@gmail.com>
2020-07-16 13:46:41 +0500

    Update Plot.java

--- graphics/src/main/java/ru/yandex/fixcolor/my_lib/graphics/Plot.java
+++ graphics/src/main/java/ru/yandex/fixcolor/my_lib/graphics/Plot.java
@@ -47,6 +47,9 @@ public class Plot {
 
     // масив графиков
     private ArrayList<Trend> trends = null;
+    private ArrayList<Short[]> dataGraphics = null;
+    private MyPaint myPaint = null;
+    private Short[] newData = null;
 
     private boolean busy = false;
 
@@ -98,6 +101,7 @@ public class Plot {
         private final BlockingQueue<DatQueue> paintQueue = new ArrayBlockingQueue<>(25);
         private Object lock = new Object();
         private boolean onWork;
+        private MyPaint thisThread = null;
 
         DatQueue queueClearFields = new DatQueue(ClearFields, null);
         DatQueue queueClearWindow = new DatQueue(ClearWindow, null);
@@ -105,6 +109,7 @@ public class Plot {
 
         @Override
         public void run() {
+            thisThread = this;
             onWork = true;
             DatQueue datQueue = null;
 
@@ -113,13 +118,13 @@ public class Plot {
                     datQueue = paintQueue.poll(1, TimeUnit.MILLISECONDS);
                     switch (datQueue.command) {
                         case ClearFields:
-                            Platform.runLater( ()-> _clearFields() );
+                            Platform.runLater( ()-> __clearFields() );
                             break;
                         case ClearWindow:
-                            Platform.runLater( ()-> _clearWindow() );
+                            Platform.runLater( ()-> __clearWindow() );
                             break;
                         case PaintNet:
-                            Platform.runLater( ()-> _paintNet() );
+                            Platform.runLater( ()-> __paintNet() );
                             break;
                         case RePaint:
                             __rePaint(datQueue.datGraph);
@@ -135,6 +140,31 @@ public class Plot {
                     break;
                 }
             }
+        }
+
+        public void clearFields() {
+            paintQueue.add(queueClearFields);
+        }
+
+        public void clearWindow() {
+            paintQueue.add(queueClearWindow);
+        }
+
+        public void paintNet() {
+            paintQueue.add(queuePaintNet);
+        }
+
+        public void rePaint(ArrayList<Short[]> dat) {
+            paintQueue.add(new DatQueue(RePaint, dat));
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            onWork = false;
+            while (!thisThread.isAlive()) {
+                Thread.yield();
+            }
+            super.finalize();
         }
 
         private void __rePaint(ArrayList<Short[]> datGraph) {
@@ -181,92 +211,103 @@ public class Plot {
             }
 
             Platform.runLater(()->{
-                _clearFields();
-                _clearWindow();
-                _paintNet();
+                __clearFields();
+                __clearWindow();
+                __paintNet();
                 for (int i = 1; i < nItemsMass; i++) {
                     trends.get(i - 1).rePaint(massGraphcs[0], massGraphcs[i]);
                 }
             });
             busy = false;
         }
+
+        private void __clearFields() {
+            gc.beginPath();
+
+            gc.setFill(fieldBackColor);
+            gc.fillRect(0, 0, fieldWidth, height);
+            gc.fillRect(0, height - fieldHeight, width, height);
+
+            double polFrameLineWidth = fieldFrameLineWidth / 2;
+            double[] x = {
+                    fieldWidth - polFrameLineWidth,
+                    fieldWidth - polFrameLineWidth,
+                    width - polFrameLineWidth
+            };
+            double[] y = {
+                    0,
+                    height - fieldHeight + polFrameLineWidth,
+                    height - fieldHeight + polFrameLineWidth
+            };
+
+            gc.setStroke(fieldFrameLineColor);
+            gc.setLineWidth(fieldFrameLineWidth);
+            gc.strokePolyline(x, y, x.length);
+
+            gc.stroke();
+            gc.closePath();
+        }
+
+        private void __clearWindow() {
+            gc.beginPath();
+            gc.setFill(windowBackColor);
+            gc.fillRect(fieldWidth, 0, width, height - fieldHeight);
+            gc.stroke();
+            gc.closePath();
+        }
+
+        private void __paintNet() {
+            double kNet;
+            if (levelYlenght <= 100) kNet = 10;
+            else if (levelYlenght <= 250) kNet = 25;
+            else if (levelYlenght <= 500) kNet = 50;
+            else if (levelYlenght <= 750) kNet = 75;
+            else if (levelYlenght <= 1000) kNet = 100;
+            else if (levelYlenght <= 1500) kNet = 150;
+            else kNet = 200;
+
+            double xSize = width - fieldWidth;
+            double ySize = height - fieldHeight;
+            int xN = 12 + 1;
+            int yN = (int)(levelYlenght / kNet) + 0;
+            double x, y, polLineWidth = netLineWidth / 2;
+
+            gc.beginPath();
+            gc.setStroke(netLineColor);
+            gc.setLineWidth(netLineWidth);
+            // x
+            for (int i = 1; i < xN - 1; i++) {
+                x = (i * xSize / (xN -1)) + fieldWidth;
+                gc.moveTo(x, fieldHeight + polLineWidth);
+                gc.lineTo(x, fieldHeight - polLineWidth + ySize);
+            }
+            // y
+            double kp = ySize / levelYlenght;
+            for (int i = 0; i < yN; i++) {
+                y = height - (kp * i * kNet);
+                gc.moveTo(fieldWidth + polLineWidth, y);
+                gc.lineTo(width - polLineWidth, y);
+            }
+
+            gc.stroke();
+            gc.closePath();
+        }
+
     }
 
     public Plot(Canvas canvas, double fieldWidth, double fieldHeight) {
         this.canvas = canvas;
         this.fieldWidth = fieldWidth;
         this.fieldHeight = fieldHeight;
+        width = canvas.getWidth();
+        height = canvas.getHeight();
+        gc = canvas.getGraphicsContext2D();
+        trends = new ArrayList<>();
+        dataGraphics = new ArrayList<>();
+        newData = new Short[0];
+
+        myPaint = new MyPaint();
+        myPaint.start();
     }
 
-    private void  _clearFields() {
-        gc.beginPath();
-
-        gc.setFill(fieldBackColor);
-        gc.fillRect(0, 0, fieldWidth, height);
-        gc.fillRect(0, height - fieldHeight, width, height);
-
-        double polFrameLineWidth = fieldFrameLineWidth / 2;
-        double[] x = {
-                fieldWidth - polFrameLineWidth,
-                fieldWidth - polFrameLineWidth,
-                width - polFrameLineWidth
-        };
-        double[] y = {
-                0,
-                height - fieldHeight + polFrameLineWidth,
-                height - fieldHeight + polFrameLineWidth
-        };
-
-        gc.setStroke(fieldFrameLineColor);
-        gc.setLineWidth(fieldFrameLineWidth);
-        gc.strokePolyline(x, y, x.length);
-
-        gc.stroke();
-        gc.closePath();
-    }
-
-    private void _clearWindow() {
-        gc.beginPath();
-        gc.setFill(windowBackColor);
-        gc.fillRect(fieldWidth, 0, width, height - fieldHeight);
-        gc.stroke();
-        gc.closePath();
-    }
-    
-    private void _paintNet() {
-        double kNet;
-        if (levelYlenght <= 100) kNet = 10;
-        else if (levelYlenght <= 250) kNet = 25;
-        else if (levelYlenght <= 500) kNet = 50;
-        else if (levelYlenght <= 750) kNet = 75;
-        else if (levelYlenght <= 1000) kNet = 100;
-        else if (levelYlenght <= 1500) kNet = 150;
-        else kNet = 200;
-        
-        double xSize = width - fieldWidth;
-        double ySize = height - fieldHeight;
-        int xN = 12 + 1;
-        int yN = (int)(levelYlenght / kNet) + 0;
-        double x, y, polLineWidth = netLineWidth / 2;
-        
-        gc.beginPath();
-        gc.setStroke(netLineColor);
-        gc.setLineWidth(netLineWidth);
-        // x
-        for (int i = 1; i < xN - 1; i++) {
-            x = (i * xSize / (xN -1)) + fieldWidth;
-            gc.moveTo(x, fieldHeight + polLineWidth);
-            gc.lineTo(x, fieldHeight - polLineWidth + ySize);
-        }
-        // y
-        double kp = ySize / levelYlenght;
-        for (int i = 0; i < yN; i++) {
-            y = height - (kp * i * kNet);
-            gc.moveTo(fieldWidth + polLineWidth, y);
-            gc.lineTo(width - polLineWidth, y);
-        }
-
-        gc.stroke();
-        gc.closePath();
-    }
 }
