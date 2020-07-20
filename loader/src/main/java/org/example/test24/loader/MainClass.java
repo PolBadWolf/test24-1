8a633649818a1f241afc1ab4d04dc0e9333971ab
PolBadWolf <gm.gameoff@gmail.com>
2020-07-20 08:46:20 +0500

    update loader

--- loader/src/main/java/org/example/test24/loader/MainClass.java
+++ loader/src/main/java/org/example/test24/loader/MainClass.java
@@ -1,10 +1,62 @@
 package org.example.test24.loader;
 
-import javafx.scene.canvas.GraphicsContext;
+import org.example.test24.RS232.CommPort;
+import org.example.test24.allinterface.Closer;
+import org.example.test24.allinterface.commPort.BAUD;
+import org.example.test24.runner.Running;
+import org.example.test24.screen.MainFrame;
+import org.example.test24.screen.ScreenClass;
 
-import java.util.ArrayList;
 
 public class MainClass {
+    private ScreenClass mainFx = null;
+    private Running runner = null;
+    private CommPort commPort = null;
+
     public static void main(String[] args) {
+        new MainClass().start(args);
+    }
+
+    private void start(String[] args) {
+        String namePort = "";
+
+        if (args.length > 0)    namePort = args[0];
+
+        mainFx= new ScreenClass();
+        runner = new Running();
+        commPort = new CommPort();
+
+        Closer.getCloser().init(commPort, runner, mainFx);
+
+        int checkComm = commPort.Open(runner, namePort, BAUD.baud115200);
+        if (checkComm != CommPort.INITCODE_OK) {
+            errorCommMessage(checkComm, commPort);
+            System.exit(0);
+        }
+
+        (new Thread(mainFx)).start();
+        while (MainFrame.mainFrame == null) {
+            Thread.yield();
+        }
+
+        runner.init(commPort, MainFrame.mainFrame);
+        //runner.start();
+
+        commPort.ReciveStart();
+    }
+
+    private void errorCommMessage(int checkComm, CommPort commPort) {
+        switch (checkComm) {
+            case CommPort.INITCODE_ERROROPEN:
+                System.out.println("ошибка открытия порта");
+                break;
+            case CommPort.INITCODE_NOTEXIST:
+                System.out.println("указанный порт отсутствует");
+                System.out.println("имеющиеся порты в системе:");
+                for (String name : commPort.getListPortsName()) {
+                    System.out.println(name);
+                }
+                break;
+        }
     }
 }
