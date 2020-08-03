ee50b67c5ac1bd135c2fdc09d81651d187c2ce2a
PolBadWolf <gm.gameoff@gmail.com>
2020-08-03 13:55:19 +0500

    Create Converts.java

---
+++ lib/src/main/java/org/example/lib/Converts.java
@@ -0,0 +1,13 @@
+package org.example.lib;
+
+public class Converts {
+    public static void IntToBytes(int source, byte[] target, int lenghtSource, int offsetTarget) {
+        int i = 0;
+        do {
+            target[offsetTarget + i] = (byte) (source & 0xff);
+            i++;
+            if (i >= lenghtSource) return;
+            source >>= 8;
+        } while (true);
+    }
+}
