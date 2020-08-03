3c6beb530ad1c27b76f3878348b91619a92e001d
PolBadWolf <gm.gameoff@gmail.com>
2020-08-03 14:08:05 +0500

    Create DistClass.java

---
+++ allinterface/src/main/java/org/example/test24/allinterface/bd/DistClass.java
@@ -0,0 +1,11 @@
+package org.example.test24.allinterface.bd;
+
+public class DistClass {
+    public int tik;
+    public int distance;
+
+    public DistClass(int tik, int distance) {
+        this.tik = tik;
+        this.distance = distance;
+    }
+}
