08a2a620a3055c301b48ada77f6f36a1892c3a26
PolBadWolf <gm.gameoff@gmail.com>
2020-08-17 12:28:22 +0500

    tuning frame: switch bdwork to bdsql

--- bd/src/main/java/org/example/bd/SqlWork_interface.java
+++ bd/src/main/java/org/example/bd/SqlWork_interface.java
@@ -4,10 +4,11 @@ import java.sql.Blob;
 import java.sql.Connection;
 import java.util.Date;
 
-interface SqlWork_interface {
+public interface SqlWork_interface {
     Connection getConnect() throws Exception;
-
+    String getTypeBD();
     void pushDataDist(Date date, long id_spec, int n_cicle, int ves, int tik_shelf, int tik_back, int tik_stop, Blob distance) throws Exception;
     boolean testStuctBase(String ip, String portServer, String login, String password, String base);
+    String[] getConnectListBd(String ip, String portServer, String login, String password) throws Exception;
     ParametersSql getParametrsSql();
 }
