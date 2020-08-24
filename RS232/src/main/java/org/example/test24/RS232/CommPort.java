3f1e4a3258b378feedb6de02703ecbbdb9099223
PolBadWolf <gm.gameoff@gmail.com>
2020-08-24 16:30:19 +0500

    fix call back interface rs232

--- RS232/src/main/java/org/example/test24/RS232/CommPort.java
+++ RS232/src/main/java/org/example/test24/RS232/CommPort.java
@@ -1,17 +1,18 @@
 package org.example.test24.RS232;
 
-import org.example.lib.interfaces.RsCallBack;
-
 public interface CommPort {
+    interface CallBack {
+        void reciveRsPush(byte[] bytes, int lenght);
+    }
+    static CommPort main() {
+        return new CommPortClass();
+    }
     String[] getListPortsName();
-    int Open(RsCallBack rsCallBack, String portName, BAUD baud);
+    int Open(CallBack callBack, String portName, BAUD baud);
     int INITCODE_OK           = 0;
     int INITCODE_NOTEXIST     = 1;
     int INITCODE_ERROROPEN    = 2;
     void Close();
     boolean ReciveStart();
     void ReciveStop();
-    public static CommPort main() {
-        return new CommPortClass();
-    }
 }
