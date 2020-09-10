5ead45443cca68172a39dca890c548cc1f2b5300
PolBadWolf <gm.gameoff@gmail.com>
2020-09-10 12:45:24 +0500

    fix my logger

--- lib/src/main/java/org/example/lib/MyLogger.java
+++ lib/src/main/java/org/example/lib/MyLogger.java
@@ -2,16 +2,15 @@ package org.example.lib;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class MyLogger {
     public static Logger myLog;
-    private final static String logFilePath = "./";
-    private final static SimpleDateFormat formatLogFile = new SimpleDateFormat("yyyy-MM-dd");
+    private final static String logFilePath = "./log/";
+    private final static SimpleDateFormat formatForNameLogFile = new SimpleDateFormat("yyyy-MM-dd");
+    private final static SimpleDateFormat formatLogFile = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public MyLogger(Level consoleLevel, Level fileLevel) {
         myLog = Logger.getLogger("Logger spc");
         myLog.setLevel(Level.ALL);
@@ -27,16 +26,36 @@ public class MyLogger {
                 handler = new FileHandler(
                         logFilePath
                         + "log_"
-                        + formatLogFile.format(Calendar.getInstance().getTime())
+                        + formatForNameLogFile.format(Calendar.getInstance().getTime())
                         + ".log"
                         , 0, 50
                 );
                 handler.setEncoding("cp1251");
+                handler.setFormatter(new Formatter() {
+                    @Override
+                    public String format(LogRecord record) {
+                        StringBuilder string =
+                                new StringBuilder(formatLogFile.format(record.getMillis())
+                                        + " Class: \"" + record.getSourceClassName() + "\""
+                                        + " Method: \"" + record.getSourceMethodName() + "\""
+                                        + "\n"
+                                        + record.getLevel().getName() + ": \"" + record.getMessage() + "\"");
+                        Object[] parameters = record.getParameters();
+                        if (parameters != null) {
+                            for (Object parameter : parameters) {
+                                StackTraceElement traceElement = (StackTraceElement) parameter;
+                                string.append("\n   ").append(traceElement.toString());
+                            }
+                        }
+                        string.append("\n---------------------------------------------------------\n");
+                        return string.toString();
+                    }
+                });
             } catch (IOException e) {
-                System.out.println("Ошибка создания файлово логера: \n" + e.getStackTrace().toString());
+                System.out.println("Ошибка создания файлово логера: \n" + Arrays.toString(e.getStackTrace()));
                 e.printStackTrace();
             }
-            handler.setLevel(fileLevel);;
+            handler.setLevel(fileLevel);
             myLog.addHandler(handler);
         }
     }
