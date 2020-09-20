59cedf2e8ada1bcf557c012d9d2e439d160115eb
PolBadWolf <gm.gameoff@gmail.com>
2020-09-20 08:58:58 +0500

    fix user class

--- bd/src/main/java/org/example/test24/bd/UserClass.java
+++ bd/src/main/java/org/example/test24/bd/UserClass.java
@@ -7,16 +7,16 @@ public class UserClass {
     final public static int RANG_PUSHERS = 1;
     public int id_user;
     public Date date;
-    public int id_userEdit;
+    public int id_loggerUser;
     public String name;
     public String password;
     public int rang;
     public Date date_unreg;
 
-    public UserClass(int id_user, Date date, int id_userEdit, String name, String password, int rang, Date date_unreg) {
+    public UserClass(int id_user, Date date, int id_loggerUser, String name, String password, int rang, Date date_unreg) {
         this.id_user = id_user;
         this.date = date;
-        this.id_userEdit = id_userEdit;
+        this.id_loggerUser = id_loggerUser;
         this.name = name;
         this.password = password;
         this.rang = rang;
