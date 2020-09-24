d6f85ad24008fdaf4f6b78bbb189c4eca96b6485
PolBadWolf <gm.gameoff@gmail.com>
2020-09-24 10:28:42 +0500

    create class pusher type

---
+++ bd/src/main/java/org/example/test24/bd/PusherType.java
@@ -0,0 +1,28 @@
+package org.example.test24.bd;
+
+import java.util.Date;
+
+public class PusherType {
+    public long id_typePusher;
+    public Date date;                   // когда правили/создали
+    public long id_loggerUser;          // кто правил/создал
+    public long id_loggerTypePusher;    // id записи
+    public String name;                 // названия типа толкателя
+    public int force;                   // номинальное усилие
+    public int move_min;                // минимальное значение номинального хода
+    public int move_max;                // максимальное значение номинального хода
+
+    public PusherType(long id_typePusher, Date date, long id_loggerUser, long id_loggerTypePusher, String name, int force, int move_min, int move_max) {
+        this.id_typePusher = id_typePusher;
+        this.date = date;
+        this.id_loggerUser = id_loggerUser;
+        this.id_loggerTypePusher = id_loggerTypePusher;
+        this.name = name;
+        this.force = force;
+        this.move_min = move_min;
+        this.move_max = move_max;
+    }
+
+    @Override
+    public String toString() { return name; }
+}
