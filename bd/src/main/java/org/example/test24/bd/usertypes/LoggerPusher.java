c9191a2ea481bde6aee0d41acb3d6bbe0f4e75ce
PolBadWolf <gm.gameoff@gmail.com>
2020-09-28 15:33:06 +0500

    create

---
+++ bd/src/main/java/org/example/test24/bd/usertypes/LoggerPusher.java
@@ -0,0 +1,12 @@
+package org.example.test24.bd.usertypes;
+
+import java.util.Date;
+
+public class LoggerPusher {
+    public long id_loggerPusher;
+    public Date date_upd;
+    public long id_loggerUserEdit;
+    public long id_pusher;
+    public String namePusher;
+    public LoggerTypePusher loggerTypePusher;
+}
