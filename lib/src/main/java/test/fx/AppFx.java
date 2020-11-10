80c8ce0d10bf3a2a17da77e7bdbf9aba9066be62
PolBadWolf <gm.gameoff@gmail.com>
2020-11-10 22:33:53 +0500

    update plot2 - create test fx

---
+++ lib/src/main/java/test/fx/AppFx.java
@@ -0,0 +1,33 @@
+package test.fx;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import ru.yandex.fixcolor.tests.spc.lib.plot2.Plot;
+
+public class AppFx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Canvas canvas = new Canvas();
+        canvas.setWidth(1000);
+        canvas.setHeight(550);
+        Parent root = new BorderPane(canvas);
+        Scene scene = new Scene(root, 1200, 700);
+        primaryStage.setTitle("fx");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Plot.Parameters parameters = new Plot.Parameters();
+        Plot plot = Plot.createFx(parameters, canvas);
+        plot.clear();
+    }
+}
---
+++ lib/src/main/java/test/fx/GraphFxTest.java
@@ -0,0 +1,10 @@
+package test.fx;
+
+public class GraphFxTest {
+    public static void main(String[] args) {
+        new GraphFxTest().start();
+    }
+    private void start() {
+        AppFx.main(null);
+    }
+}
